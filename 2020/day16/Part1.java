43fda79e8b21a6439d78bab9d3a6769c2ea5a622
Juergen Reuter <lily-devel@soundpaint.org>
2020-12-16 20:53:50 +0100

    Temporary Solution #3 for Day 16
    * Basic clean-up, but still both parts
      in one file.
    * 5-lines performance fix (from ~290s to
      ~25ms on my machine).

--- 2020/day16/Part1.java
+++ 2020/day16/Part1.java
@@ -1,32 +1,36 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Part1
 {
-  private Part1() {}
+  private final ArrayList<Rule> rules;
+  private final ArrayList<Ticket> tickets;
+  private final ArrayList<Rule> sortedRules;
 
-  private ArrayList<Rule> rules;
-  private ArrayList<Ticket> tickets;
-  private ArrayList<Rule> sortedRules;
+  private Part1() {
+    rules = new ArrayList<Rule>();
+    tickets = new ArrayList<Ticket>();
+    sortedRules = new ArrayList<Rule>();
+  }
 
-  private class Rule
+  private static class Rule
   {
-    String id;
-    long range1Min;
-    long range1Max;
-    long range2Min;
-    long range2Max;
-    boolean selected;
-    boolean applicableForColumn[];
+    private final String id;
+    private final long range1Min;
+    private final long range1Max;
+    private final long range2Min;
+    private final long range2Max;
+    private boolean applicableForColumn[];
+    private int applicableForColumns;
+    private boolean selected;
 
-    Rule(String id, long range1Min, long range1Max, long range2Min, long range2Max)
+    public Rule(final String id,
+                final long range1Min, final long range1Max,
+                final long range2Min, final long range2Max)
     {
       this.id = id;
       this.range1Min = range1Min;
@@ -35,21 +39,39 @@ public class Part1
       this.range2Max = range2Max;
       selected = false;
     }
-    boolean check(long value)
+
+    public final boolean satisfiesRangeConstraints(final long value)
     {
       return
         ((value >= range1Min) && (value <= range1Max)) ||
         ((value >= range2Min) && (value <= range2Max));
     }
-    String getDescription() {
-      return range1Min + "-" + range1Max + " or " + range2Min + "-" + range2Max;
+
+    public static Rule parse(final String unparsed)
+    {
+      final String[] ruleTokens = unparsed.split(":");
+      final String id = ruleTokens[0].trim();
+      final String[] rangesTokens = ruleTokens[1].trim().split(" or ");
+      final String[] range1Tokens = rangesTokens[0].trim().split("-");
+      final long range1Min = Long.parseLong(range1Tokens[0].trim());
+      final long range1Max = Long.parseLong(range1Tokens[1].trim());
+      final String[] range2Tokens = rangesTokens[1].trim().split("-");
+      final long range2Min = Long.parseLong(range2Tokens[0].trim());
+      final long range2Max = Long.parseLong(range2Tokens[1].trim());
+      return new Rule(id, range1Min, range1Max, range2Min, range2Max);
+    }
+
+    public String toString() {
+      return id + ": " +
+        range1Min + "-" + range1Max + " or " + range2Min + "-" + range2Max;
     }
   }
 
   private class Ticket
   {
-    long[] values;
-    Ticket(String[] values)
+    private final long[] values;
+
+    public Ticket(final String[] values)
     {
       this.values = new long[values.length];
       for (int i = 0; i < values.length; i++) {
@@ -57,55 +79,41 @@ public class Part1
       }
     }
 
-    private boolean ruleApplicableForColumn(Rule rule, int column)
+    private boolean ruleApplicableForColumn(final Rule rule, final int column)
     {
-      boolean applicable = rule.check(values[column]);
-      /*
-      System.out.println("checking " + values[column] + " against " +
-                         rule.id + ": " + applicable);
-      */
-      /*
-      if (!applicable) {
-        System.out.println("rule " + rule.id +
-                           " not applicable for column " + column + ": " +
-                           values[column] + " not in " + rule.getDescription());
-      }
-      */
-      return applicable;
+      return rule.satisfiesRangeConstraints(values[column]);
     }
 
-    private void instantiate()
+    private long instantiate()
     {
-      long result = 1;
+      long product = 1;
       for (int i = 0; i < values.length; i++) {
         String fieldName = sortedRules.get(i).id;
         long fieldValue = values[i];
-        System.out.println(fieldName + "=" + fieldValue);
         if (fieldName.startsWith("departure")) {
-          result *= fieldValue;
+          product *= fieldValue;
         }
       }
-      System.out.println("result=" + result);
+      return product;
     }
   }
 
-  private Rule check(long value)
+  private boolean isValidForSomeRule(final long value)
   {
     if (value < 0) throw new RuntimeException();
-    for (Rule rule : rules) {
-      if (rule.check(value)) return rule;
+    for (final Rule rule : rules) {
+      if (rule.satisfiesRangeConstraints(value)) return true;
     }
-    return null;
+    return false;
   }
 
-  private long check(String[] values)
+  private long getTicketScanningErrorRate(final String[] values)
   {
     long sum = 0;
     boolean isValid = true;
-    for (String value : values) {
-      long longValue = Long.parseLong(value);
-      Rule rule = check(longValue);
-      if (rule == null) {
+    for (final String value : values) {
+      final long longValue = Long.parseLong(value);
+      if (!isValidForSomeRule(longValue)) {
         sum += longValue;
         isValid = false;
       }
@@ -113,12 +121,13 @@ public class Part1
     return isValid ? -1 : sum;
   }
 
-  private boolean ruleApplicableForAllTickets(Rule rule, int column)
+  private boolean ruleApplicableForAllTicketsForColumn(final Rule rule,
+                                                       final int column)
   {
     if (rule.applicableForColumn != null) {
       return rule.applicableForColumn[column];
     }
-    for (Ticket ticket : tickets) {
+    for (final Ticket ticket : tickets) {
       if (!ticket.ruleApplicableForColumn(rule, column)) {
         return false;
       }
@@ -126,46 +135,12 @@ public class Part1
     return true;
   }
 
-  private String unselectedRulesAsString(ArrayList<Rule> rules)
+  private boolean determineRules(final int startColumn)
   {
-    StringBuffer s = new StringBuffer();
-    for (int i = 0; i < rules.size(); i++) {
-      if (rules.get(i).selected) continue;
-      if (s.length() > 0) s.append(", ");
-      s.append(rules.get(i).id);
-    }
-    return s.toString();
-  }
-
-  private String rulesAsString(ArrayList<Rule> rules)
-  {
-    StringBuffer s = new StringBuffer();
-    for (int i = 0; i < rules.size(); i++) {
-      if (s.length() > 0) s.append(", ");
-      s.append(rules.get(i).id);
-    }
-    return s.toString();
-  }
-
-  private boolean determineRules(int startColumn)
-  {
-    System.out.println("sorted rules(" + startColumn + "): " +
-                       rulesAsString(sortedRules));
-    /*
-    System.out.println("remaining rules(" + startColumn + "): " +
-                       unselectedRulesAsString(rules));
-    */
     if (startColumn >= rules.size()) return true;
-    for (Rule rule : rules) {
-      if (rule.selected) {
-        System.out.println("skipping " + rule.id);
-        continue;
-      } else {
-        System.out.println("checking " + rule.id);
-      }
-      //System.out.println("checking rule " + rule.id);
-      if (ruleApplicableForAllTickets(rule, startColumn)) {
-        //System.out.println("found rule: " + rule.id);
+    for (final Rule rule : rules) {
+      if (rule.selected) continue;
+      if (ruleApplicableForAllTicketsForColumn(rule, startColumn)) {
         sortedRules.add(rule);
         rule.selected = true;
         if (determineRules(startColumn + 1))
@@ -174,51 +149,34 @@ public class Part1
         sortedRules.remove(sortedRules.size() - 1);
       }
     }
-    System.out.println("sorted rules(" + startColumn + "): " +
-                       "no solution found");
     return false;
   }
 
-  private void run1(final String filePath) throws IOException
+  private void parseRules(final BufferedReader reader) throws IOException
   {
-    rules = new ArrayList<Rule>();
-    final var reader = new BufferedReader(new FileReader(filePath));
     String line;
-    // rules
-    while ((line = reader.readLine()) != null) {
-      if (line.isEmpty()) break;
-      String[] tokens = line.split(":");
-      String id = tokens[0].trim();
-      System.out.println("rule id=" + id);
-      String ranges = tokens[1].trim();
-      tokens = ranges.split(" or ");
-      String range1 = tokens[0].trim();
-      String range2 = tokens[1].trim();
-      tokens = range1.split("-");
-      long range1Min = Long.parseLong(tokens[0].trim());
-      long range1Max = Long.parseLong(tokens[1].trim());
-      tokens = range2.split("-");
-      long range2Min = Long.parseLong(tokens[0].trim());
-      long range2Max = Long.parseLong(tokens[1].trim());
-      Rule rule = new Rule(id, range1Min, range1Max, range2Min, range2Max);
-      rules.add(rule);
+    while (!(line = reader.readLine()).isEmpty()) {
+      rules.add(Rule.parse(line));
     }
-    System.out.println("unsorted rules: " + rulesAsString(rules));
+  }
 
-    String[] values;
+  private Ticket parseYourTicket(final BufferedReader reader) throws IOException
+  {
+    reader.readLine();
+    final Ticket ticket = new Ticket(reader.readLine().split(","));
+    reader.readLine();
+    return ticket;
+  }
 
-    // your ticket
-    line = reader.readLine();
-    Ticket yourTicket = new Ticket(reader.readLine().split(","));
-    line = reader.readLine();
-
-    // nearby tickets
-    tickets = new ArrayList<Ticket>();
-    line = reader.readLine();
+  private void parseNearbyTickets(final BufferedReader reader)
+    throws IOException
+  {
+    reader.readLine();
     long sum = 0;
+    String line;
     while ((line = reader.readLine()) != null) {
-      values = line.split(",");
-      long singleSum = check(values);
+      final String[] values = line.split(",");
+      final long singleSum = getTicketScanningErrorRate(values);
       if (singleSum == -1) {
         tickets.add(new Ticket(values));
       } else {
@@ -226,51 +184,60 @@ public class Part1
       }
     }
     System.out.println(sum);
+  }
 
-    for (Rule rule : rules) {
-      boolean[] applicableForColumn = new boolean[rules.size()];
+  private void computeWhichRulesApplyOnWhatColumns()
+  {
+    for (final Rule rule : rules) {
+      final boolean[] applicableForColumn = new boolean[rules.size()];
+      int count = 0;
       for (int column = 0; column < rules.size(); column++) {
-        boolean applicable = ruleApplicableForAllTickets(rule, column);
-        applicableForColumn[column] = applicable;
-        if (applicable) {
-          System.out.println("rule " + rule.id +
-                             " applicable for column " + column + ": " +
-                             applicable);
+        if (applicableForColumn[column] =
+            ruleApplicableForAllTicketsForColumn(rule, column)) {
+          count++;
         }
       }
       rule.applicableForColumn = applicableForColumn;
+      rule.applicableForColumns = count;
     }
-    // System.exit(0);
-
-    System.out.println("found " + tickets.size() + " valid tickets");
-    sortedRules = new ArrayList<Rule>();
-    if (determineRules(0)) {
-      System.out.println("sorted rules:");
-      for (Rule rule : sortedRules) {
-        System.out.println(rule.id);
-      }
-      System.out.println();
-      yourTicket.instantiate();
-    } else {
-      System.out.println("no solution found");
-    }
-    // wrong answer: 761724660253
-    // wrong answer: 939191599769
   }
 
-  private void run2(final String filePath) throws IOException
+  /**
+   * Performance optimization: Rearrange order of rules such that in
+   * the resulting search tree for a depth first search, the top level
+   * nodes have smallest degree, while the bottom level nodes have
+   * highest degree.
+   *
+   * At least for my input data, this approach reduces runtime costs
+   * on my machine from roughly 290s to less than roughly 25ms, that is
+   * by a factor of more than 10000.
+   */
+  private void sortRules()
   {
-    final var path = Paths.get(filePath);
-    final var data =
-      new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
-    final String values[] = data.trim().split("[,\\s]");
-    for (final var value : values) {
-      System.out.println("[" + value + "]");
+    Collections.sort(rules, (lhs, rhs) -> {
+        return
+          lhs.applicableForColumns > rhs.applicableForColumns ? 1 :
+          (lhs.applicableForColumns < rhs.applicableForColumns ? -1 : 0);
+      });
+  }
+
+  private void run(final String filePath) throws IOException
+  {
+    final var reader = new BufferedReader(new FileReader(filePath));
+    parseRules(reader);
+    final Ticket yourTicket = parseYourTicket(reader);
+    parseNearbyTickets(reader);
+    computeWhichRulesApplyOnWhatColumns();
+    sortRules();
+    if (determineRules(0)) {
+      System.out.println(yourTicket.instantiate());
+    } else {
+      System.out.println("no solution found");
     }
   }
 
   public static void main(final String argv[]) throws IOException
   {
-    new Part1().run1("data.txt");
+    new Part1().run("data.txt");
   }
 }
